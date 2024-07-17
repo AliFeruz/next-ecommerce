7939426ce0fcfaa60414607b20d6b8c132ab4943
Ali Feruz <aliferuzcode@gmail.com>
2024-07-17 23:38:47 +0200

    updated single item page

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -9,11 +9,12 @@ const CategoryList = async () => {
     .collections
     .queryCollections()
     .find()
+
   return (
     <div className='px-4 overflow-x-scroll hide-scrollbar'>
       <div className='flex gap-4 md:gap-8'>
         {cat.items.map((item, index) => (
-            <Link href={`/list?cat=${item.slug}`} className='flex-shrink-0 w-full sm:w-1/2 lg:1/4 xl:w-1/6' key={index}>
+            <Link  href={`/list?cat=${item.slug}`} className='flex-shrink-0 w-full sm:w-1/2 lg:1/4 xl:w-1/6' key={index}>
             <div className='relative bg-slate-200/50 w-full h-96'>
                 <Image src={item.media?.mainMedia?.image?.url || '/product.png'} 
                 alt='image' fill sizes='20vw'
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -2,41 +2,41 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 
-const images = [
-    {
-        id: 1,
-        url: 'https://images.pexels.com/photos/26898422/pexels-photo-26898422/free-photo-of-a-spiral-staircase-with-a-metal-railing.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-    },
-    {
-        id: 2,
-        url: 'https://images.pexels.com/photos/26889959/pexels-photo-26889959/free-photo-of-a-park-bench-is-sitting-in-the-middle-of-a-park.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-    },
-    {
-        id: 3,
-        url: 'https://images.pexels.com/photos/9116768/pexels-photo-9116768.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-    },
-    {
-        id: 1,
-        url: 'https://images.pexels.com/photos/14108380/pexels-photo-14108380.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-    },
-]
+// const images = [
+//     {
+//         id: 1,
+//         url: 'https://images.pexels.com/photos/26898422/pexels-photo-26898422/free-photo-of-a-spiral-staircase-with-a-metal-railing.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+//     },
+//     {
+//         id: 2,
+//         url: 'https://images.pexels.com/photos/26889959/pexels-photo-26889959/free-photo-of-a-park-bench-is-sitting-in-the-middle-of-a-park.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+//     },
+//     {
+//         id: 3,
+//         url: 'https://images.pexels.com/photos/9116768/pexels-photo-9116768.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+//     },
+//     {
+//         id: 1,
+//         url: 'https://images.pexels.com/photos/14108380/pexels-photo-14108380.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+//     },
+// ]
 
-const ProductImages = () => {
+const ProductImages = ({images} : {images : any }) => {
     const [index, setIndex] = useState(0)
   return (
     <div>
       <div className='h-[500px] relative'>
-        <Image src={images[index].url}
+        <Image src={images[index].image?.url}
         alt='product image'
         fill
         sizes='50vw'
         className='object-cover rounded-md'/>
       </div>
       <div className='flex gap-4 mt-8'>
-        {images.map((item, index) => (
+        {images.map((item:any, index: number) => (
             <div className='relative w-1/4 h-32 cursor-pointer' key={index}
             onClick={() => setIndex(index)}> 
-            <Image src={item.url}
+            <Image src={item.image.url}
             alt='product image'
             fill
             sizes='50vw'
